8efb987daa3fddd007bb790a014f641ec4f35c78
MJ <mercyorangi2@gmail.com>
2024-01-11 12:23:36 +0300

    palindrome solution

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        half = 0
+        while (x > half):
+            half = half * 10 + x % 10
+            x = x // 10
+
+        if x == half or x == half // 10:
+            return True
+
+    print(isPalindrome(12321))
