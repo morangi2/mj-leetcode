6d07070a4aed76cead799f04b36f7a3f38f72adc
MJ <mercyorangi2@gmail.com>
2024-01-31 08:35:44 -0500

    removes an element from an array

---
+++ removeElement.py
@@ -0,0 +1,28 @@
+
+def removeElement(numsList, val):
+    """
+    :type numsList: List[int]
+    :type val: int
+    :rtype: int
+    """
+
+    """
+    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+    """
+
+    count = 0
+
+    for i in range(len(numsList)):
+        if numsList[i] != val:
+            numsList[count] = numsList[i]
+            count += 1
+
+    return count
+
+
+print(removeElement([3, 2, 2, 4, 3], 3))
