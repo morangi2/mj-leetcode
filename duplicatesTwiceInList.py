51ec46722709c8dd6d2fa7adbe5a4b6453b9931d
MJ <mercyorangi2@gmail.com>
2024-02-04 14:30:59 -0500

    adds explanation to code

--- duplicatesTwiceInList.py
+++ duplicatesTwiceInList.py
@@ -3,14 +3,16 @@ def removeDuplicates(nums):
     :type nums: List[int]
     :rtype: int
     """
-    occ = 2
+    k = 2
 
     for i in range(2, len(nums)):
-        if nums[i] != nums[occ-2]:
-            nums[occ] = nums[i]
-            occ += 1
+        if nums[i] != nums[k-2]:
+            nums[k] = nums[i]
+            k += 1
 
-    return occ
+    return k
 
 
 print(removeDuplicates([1, 1, 1, 2, 2, 3]))  # 5
+
+# the above is a 2-pointer technique that efficiently removes duplicates, in-place, after they occur twice, from a sorted array
