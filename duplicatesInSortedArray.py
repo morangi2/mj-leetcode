f7a5872eeb1c071c81ceb201b682689c16131c55
MJ <mercyorangi2@gmail.com>
2024-02-03 20:59:56 -0500

    removes duplicates in-place from a sorted list

---
+++ duplicatesInSortedArray.py
@@ -0,0 +1,19 @@
+
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    k = 0  # tracks the position of the last unique element
+
+    for i in range(1, len(nums)):
+        if nums[k] != nums[i]:
+            k += 1
+            nums[k] = nums[i]
+
+    return k + 1
+
+
+print(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]))  # 6
+
+# the above is a 2-pointer technique that efficiently removes duplicates, in-place, from a sorted array
