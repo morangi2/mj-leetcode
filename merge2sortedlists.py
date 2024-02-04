9120015d0cc65af2b14a695865b78abce370c01a
MJ <mercyorangi2@gmail.com>
2024-02-04 15:26:59 -0500

    adds explandation to the code implementation

--- merge2sortedlists.py
+++ merge2sortedlists.py
@@ -1,46 +1,38 @@
+# Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-
-class Solution(object):
-    def mergeTwoLists(l1, l2):
-
-        l1: ListNode
-        l2: ListNode
-        # rtype: ListNode
-
-        head = ListNode()
-        current = head
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
-
-            current = current.next  # advance current at the end of each loop
-
-        current.next = l1 or l2  # if l1 is not empty, current.next = l1, else current.next = l2
-
-        return head.next
+# first, create a new ListNode object to serve as the head of the merged list
+# then, create a new ListNode object to serve as the current pointer
+# while list1 and list2 are not empty, compare the first elements of list1 and list2
+# add the smaller of the two to the merged list
+# move the current pointer to the next element
+# repeat the process until list1 and list2 are empty
+# add the remaining elements of list1 or list2 to the merged list
+# return the merged list
 
 
-""" def arrtolinkedlist(arr):
-    res = [ListNode(x) for x in arr]
-    for i in range(len(res) - 1):
-        res[i].next = res[i + 1]
-    return res[0]
+def merge2SortedLists(list1, list2):
+    head = ListNode()
+    current = head
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
 
-ln1 = arrtolinkedlist(array1)
+        current = current.next
 
-ln2 = arrtolinkedlist(array2) """
+    current.next = list1 or list2
 
-    array1 = [1, 2, 4]
-    array2 = [1, 3, 4]
+    return head.next
 
-    print(Solution().mergeTwoLists(array1, array2))
-    # to run use: python3 merge2sortedlists.py
+# Time complexity: O(n + m)
+# Space complexity: O(1)
+
+
+print(merge2SortedLists([1, 2, 4], [1, 3, 4]))  # [1, 1, 2, 3, 4, 4]
