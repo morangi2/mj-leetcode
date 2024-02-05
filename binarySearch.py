757ed9e946808536ceea85446c36955393d3d447
MJ <mercyorangi2@gmail.com>
2024-02-05 09:05:13 -0500

    implements a binary search

---
+++ binarySearch.py
@@ -0,0 +1,27 @@
+def binarySearch(nums, target):
+    left_pointer = 0
+    right_pointer = len(nums) - 1
+
+    while left <= right:
+        mid = (left_pointer + right_pointer) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+# the above is a binary search algorithm that searches for a target in a sorted array
+# first, we define the left and right pointers
+# then we calulate the mid-point
+# if the mid-point is the target, immediately return the mid-point
+# if the mid point is less that the tartget, adjust the left pointer to mid + 1
+# if the mid point is greater than the target, adjust the right pointer to mid - 1
+# if the target is not found, return -1
+
+
+print(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))  # 4
+print(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10))  # -1
