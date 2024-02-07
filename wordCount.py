ba481d1c3cc4da9834b1ff04f2b089feeef3b79b
MJ <mercyorangi2@gmail.com>
2024-02-07 14:16:45 -0500

    adds more comments

--- wordCount.py
+++ wordCount.py
@@ -7,9 +7,10 @@ def wordCount(input_string):
         one_word = one_word.strip(".,!?")  # should not count punctuation
         one_word = one_word.lower()  # to ensure the function is case insensitive
 
-        if one_word in stop_words:
+        if one_word in stop_words:  # check if the word is in the stop_words list, and skip the word if it is
             continue
 
+        # call .get() method on the dict to get the value of the key, if the key is not in the dict, return 0
         output_dict[one_word] = output_dict.get(one_word, 0) + 1
 
     return output_dict
