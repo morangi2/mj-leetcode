18491a48240f5b8beeed4ba935c4386bbf9b1e8e
mercy <mercyorangi2@gmail.com>
2024-02-28 16:21:50 -0500

    uses double pointer to check for duplicates

--- duplicatesInSortedArray.py
+++ duplicatesInSortedArray.py
@@ -11,9 +11,30 @@ def removeDuplicates(nums):
             k += 1
             nums[k] = nums[i]
 
-    return k + 1
+    return nums[:k+1]
 
 
-print(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]))  # 6
+# [1, 2, 3, 4, 5, 6]
+print(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]))
 
 # the above is a 2-pointer technique that efficiently removes duplicates, in-place, from a sorted array
+
+
+def checkIfDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    k = 0  # tracks the position of the last unique element
+
+    for i in range(1, len(nums)):
+        if nums[k] != nums[i]:
+            k += 1
+            nums[k] = nums[i]
+        else:
+            return True
+
+    return False
+
+
+print(checkIfDuplicates([1, 2, 3, 4, 5, 6]))  # True
