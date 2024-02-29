7034e095995ab5ee786bcc67513c28d15e817f3c
MJ <mercyorangi2@gmail.com>
2024-02-29 13:00:00 -0500

    palindroms

--- palindromPermutation266.py
+++ palindromPermutation266.py
@@ -1,4 +1,31 @@
 def canPermutePalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+
+    # put is in a Counter
+    # this returns a dict with the count of each char
+    # if the length of the string is even, all the chars must be even
+    # if the length of the string is odd, only one char can be odd
+    # loop through the dict and count the odd chars
+    # if the count is more than 1, return False
+    # else return True
+
+    """
+    from collections import Counter
+    count = Counter(s)
+    print(count)  # Counter({'a': 2, 'r': 2, 'c': 2, 'e': 1})
+    odd = 0
+    for i in count:
+        if count[i] % 2 != 0:
+            odd += 1
+    return odd <= 1
+
+    """
+
+    # OR
+
     n = len(s)
     my_dict = {}
 
@@ -10,22 +37,13 @@ def canPermutePalindrome(s):
         else:
             my_dict[c] = 1
 
-    if n % 2 == 0:
-        for i in my_dict:
-            if my_dict[i] % 2 != 0:
-                return False
+    count = 0
 
-    elif n % 2 != 0:  # can have only 1 odd char
-        # keep a count of odd char
-        count = 0
-        if count <= 1:
-            for i in my_dict:
-                if my_dict[i] % 2 != 0:
-                    count += 1
-        else:
-            return False
+    for i in my_dict:
+        if my_dict[i] % 2 != 0:  # if the count is odd
+            count += 1
 
-    return True
+    return count <= 1
 
 
 print(canPermutePalindrome("carerac"))
